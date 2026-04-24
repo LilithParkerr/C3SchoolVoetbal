4829e10d89e334b9cf98a2950f94c7dfd8c1a485
BoredxManiac <Manicalsome.1@gmail.com>
2026-04-24 17:59:08 +0200

    grid toegepast aan beide dashboards

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,39 +7,41 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
-    <div class="Welkomtext p-[50px]  w-[20%]">
+<body class="bg-[#c2c5aa]">
+    <main>
+    <div class="mx-auto w-[85%]">
+    <div class="Welkomtext p-[50px]">
 
     <h1 class="text-center text-xl pr-[70px]">Hello user</h1>
     </div>
+    <div id="container" class="grid grid-cols-2 grid-rows-2 gap-[5px] justify-items-center items-center">
+        <div id="Txt" class="text-center w-[90%]">
+            <h1 class="helloh1">loremp ipsum</h1>
+            <p class="hellotxt">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, doloremque voluptatem sit quae aliquid nulla accusamus omnis repellat fuga inventore, nostrum, praesentium porro et velit molestiae. Accusamus, quod. Ad, dicta.</p>
+        </div>
 
-    <div class="HELLO">
-        <h1 class="helloh1">loremp ipsum</h1>
-        <p class="hellotxt">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, doloremque voluptatem sit quae aliquid nulla accusamus omnis repellat fuga inventore, nostrum, praesentium porro et velit molestiae. Accusamus, quod. Ad, dicta.</p>
+        <div class="img">
+            <img src="{{ asset('images/images.jpg') }}" alt="images">
+        </div>
+
+        <div class="Tables">
+            <h1 class="tablesh1">lorem</h1>
+            <thead>
+                <th>
+                    <p>e</p>
+                </th>
+            </thead>
+        </div>
+
+        <div class="list">
+            <h1 class="listh1">loremp ipsum</h1>
+        <ul>
+            <li>ITEM</li>
+        </ul>
+        </div>
     </div>
-
-    <div class="img">
-        <img src="" alt="">
     </div>
-
-    <div class="Tables">
-        <h1 class="tablesh1">lorem</h1>
-        <thead>
-            <th>
-                <p>e</p>
-            </th>
-        </thead>
-    </div>
-
-       <div class="list">
-        <h1 class="listh1">loremp ipsum</h1>
-       <ul>
-        <li>ITEM</li>
-       </ul>
-    </div>
-
-
-
+    </main>
 </body>
 </html>
 </x-layouts.app>
