edb835d26d11391cd9476465382e191f34753203
LilithParkerr <drp0016@edu.curio.nl>
2026-04-26 22:36:53 +0200

    introtext for homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,35 @@
 <x-layouts.guest>
-    <h1 class="flex justify-center">@auth
-    <span class="text-sm font-medium text-gray-800">
-        Hello, {{ Auth::user()->name }}!
-    </span>
-@else
-    <span class="text-sm font-medium text-gray-800">
-        Welcome
-    </span>
-@endauth</h1>
+    <section class="bg-[#3E436F]/50 p-2 mb-4">
+    <h2 class="flex justify-center font-bold">VoetbalVandaag is het centrale overzicht voor schoolvoetbal. Teams, wedstrijden, uitslagen — alles op één plek, altijd up-to-date.</h2>
+</section>
+
+<div class="flex items-center gap-6 p-6">
+    <div class="w-1/4">
+        <img src="{{ asset('images/images.jpg') }}" alt="Left image" class="w-full h-48 object-cover rounded">
+    </div>
+
+    <div class="flex flex-col gap-3 w-1/2">
+        <h1 class="text-2xl font-bold">
+            @auth
+            <span class=" font-medium text-gray-800">
+                Welkom {{ Auth::user()->name }} bij VoetbalVandaag!
+            </span>
+            @else
+            <span class="font-medium text-gray-800">Welkom bij VoetBalVandaag
+            </span>
+            @endauth</h1>
+        <p class="text-gray-600">VoetbalVandaag is dé plek voor schoolvoetbal — van de aanmelding van teams tot de allerlaatste wedstrijduitslagen. Bekijk welke teams meedoen, plan je wedstrijden en houd de resultaten bij. Alles wat je nodig hebt, vind je hier.</p>
+
+    </div>
+
+    <div class="w-1/4">
+        <img src="{{ asset('images/images.jpg') }}" alt="Right image" class="w-full h-48 object-cover rounded">
+    </div>
+</div>
+
+
+
+
+
+
 </x-layouts.guest>
