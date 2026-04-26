53343c29c76012b700b50e7701ec5bcdfdc11dd4
LilithParkerr <drp0016@edu.curio.nl>
2026-04-26 16:24:32 +0200

    fixed the NameOfUser commit before

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,11 @@
 <x-layouts.guest>
-    <h1 class="flex justify-center">Welcome {{ Auth::user()->name }}!</h1>
+    <h1 class="flex justify-center">@auth
+    <span class="text-sm font-medium text-gray-800">
+        Hello, {{ Auth::user()->name }}!
+    </span>
+@else
+    <span class="text-sm font-medium text-gray-800">
+        Welcome
+    </span>
+@endauth</h1>
 </x-layouts.guest>
