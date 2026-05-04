42b59b524e89067909d29d7b4e3be5dce8d334d3
BoredxManiac <Manicalsome.1@gmail.com>
2026-05-04 09:36:32 +0200

    Revert "Text dikte aangepast + variable functie toegevoeg aan de 'hello user' functie"
    This reverts commit 80615935e4d9a91517c14acb2314bfe34ab62eab.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,19 +11,19 @@
     <div class="mx-auto w-[85%]">
     <div class="Welkomtext p-[50px]">
 
-    <h1 class="text-center text-xl pr-[70px] font-bold">Hello  {{ auth()->user()->name }}</h1>
+    <h1 class="text-center text-xl pr-[70px]">Hello user</h1>
     </div>
-    <div id="container" class="grid grid-cols-2 grid-rows-2 gap-4 p-4 items-center">
-        <div id="Txt" class="text-center w-[90%] ">
-            <h1 class="font-bold">Info</h1>
-            <p  class="font-light">Here are all the info about the games!</p>
+    <div id="container" class="grid grid-cols-2 grid-rows-2 gap-8 p-8 justify-items-center items-center">
+        <div id="Txt" class="text-center w-[90%]">
+            <h1 class="helloh1">loremp ipsum</h1>
+            <p class="hellotxt">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, doloremque voluptatem sit quae aliquid nulla accusamus omnis repellat fuga inventore, nostrum, praesentium porro et velit molestiae. Accusamus, quod. Ad, dicta.</p>
         </div>
 
          <div id="ayay" class="">
-         <h1 class="text-center font-bold">List of games</h1>
-         <div id="color">
+         <h1 class="text-center">List of games</h1>
+         <div id="color"class="bg-[#FFFFFF]">
          <table class="w-full table-fixed">
-          <thead class="bg-[#FFFFFF]">
+          <thead>
               <th class="text-center">game</th>
               <th class="text-center">game</th>
               <th class="text-center">game</th>
@@ -31,11 +31,7 @@
           <tbody>
             @foreach ($games as $game)
                   <tr>
-                     <tr>
-                    <td>{{ $game->team1->name }}</td>
-                    <td>{{ $game->team1_score }} - {{ $game->team2_score }}</td>
-                    <td>{{ $game->team2->name }}</td>
-                    </tr>
+                    <td class="">{{ $game->team1}}</td>
                   </tr>
             @endforeach
           </tbody>
@@ -43,24 +39,24 @@
       </div>
       </div>
 
-        <div class="img w-full place-items-center">
-            <img src="{{ asset('images/images.jpg') }}" alt="images" class="w-[50%] h-auto">
+        <div class="img">
+            <img src="{{ asset('images/images.jpg') }}" alt="images">
         </div>
 
-        <div id="lol">
+        <div id="lol" class="bg-[#FFFFFF]">
         <table class="w-full table-fixed">
-          <thead class="bg-[#FFFFFF]">
+          <thead>
               <th class="text-center">game</th>
               <th class="text-center">game</th>
               <th class="text-center">game</th>
           </thead>
           <tbody>
             @foreach ($games as $game)
-                <tr>
-                <td>{{ $game->team1->name }}</td>
-                <td>{{ $game->team1_score }} - {{ $game->team2_score }}</td>
-                <td>{{ $game->team2->name }}</td>
-                </tr>
+                  <tr>
+                    <td class="">{{ $game->team1}}</td>
+                    <td class="">{{ $game->team1_score}} - {{ $game->team2_score}}</td>
+                    <td class="">{{ $game->team2}}</td>
+                  </tr>
             @endforeach
           </tbody>
       </table>
