712e8e35dfa3e040a242d19f3e98a180891a2594
sd-eman <emanirfan06@gmail.com>
2026-06-08 09:18:23 +0200

    naam van de website

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>C3-Voetbal</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-[#c2c5aa]">
